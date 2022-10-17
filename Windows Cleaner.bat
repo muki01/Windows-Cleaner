e1d75e95aa17137c37b4d9ec1944824cb0850632
Muksin Muksin <75759731+muki01@users.noreply.github.com>
2022-10-17 12:16:51 +0300

    Add files via upload

---
+++ Windows Cleaner.bat
@@ -0,0 +1,20 @@
+mode 33, 4
+@echo off
+color a
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    @RD /S /Q "C:\Windows\Temp"
+    @RD /S /Q "C:\Windows\Prefetch"
+    @RD /S /Q "%userprofile%\AppData\Local\Temp"
+    cls
+echo   -----------------------------
+echo   Cleaning Succesfuly Completed
+echo   -----------------------------
+ping localhost -n 3 >nul
+    exit
+) ELSE (
+   echo Run this Program As Administrator ...
+   PAUSE
+   EXIT
+)
